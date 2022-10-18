2e58a7657e5f7555d23dab2053f371cec1b98383
jquispe <jquispe@192.168.1.26>
2022-10-18 12:07:47 -0500

    Primer comit al repositorio github

---
+++ Prueba1/src/main/java/idat/edu/pe/daw2/test/Prueba1Application.java
@@ -0,0 +1,14 @@
+package idat.edu.pe.daw2.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba1Application.class, args);
+		System.out.println("Probando mi aplicacion Spring boot... hello world!!");
+	}
+
+}
